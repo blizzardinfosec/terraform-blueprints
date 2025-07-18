ef83e438dc59a977700154ae1234784bc6dc7181
blizzardinfosec <blizzardinfosec@protonmail.com>
2025-07-18 09:16:12 -0400

    Create variables.tf

---
+++ examples/logscale_query/variables.tf
@@ -0,0 +1,9 @@
+variable "client_id" {
+  description = "CrowdStrike API Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "CrowdStrike API Client Secret"
+  type        = string
+}
